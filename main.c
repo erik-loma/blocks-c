ca3e0e321bce584635175f01514a725673ce712e
Erik Loma <erik.loma1998@gmail.com>
2025-07-06 21:23:50 +0200

    imported OpenGL and compiled test code

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
+#include <GL/freeglut.h>
 #include <stdio.h>
 
-int main(void) {
-    printf("My first compile of the project!\n");
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+        glColor3f(1.0, 0.0, 0.0);
+        glVertex2f(-0.5, -0.5);
+        glColor3f(0.0, 1.0, 0.0);
+        glVertex2f(0.5, -0.5);
+        glColor3f(0.0, 0.0, 1.0);
+        glVertex2f(0.0, 0.5);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutCreateWindow("OpenGL in C");
+    glutDisplayFunc(display);
+    glutMainLoop();
     return 0;
 }
